ac803a6d14d2ded0a68feca4175938ff050ef8ec
paul1339 <110659900+paul1339@users.noreply.github.com>
2022-11-11 17:27:47 +0000

    update

--- block.sh
+++ block.sh
@@ -10,18 +10,18 @@ numargs=$#
 
 # Check if we have the right number of args
 if [ $numargs -lt 1 ]; then
-    echo "Usage: $0 <init | flush | check | ping1>"
+    echo "Usage: $0 <init | flush | check | ping>"
     exit 1
 fi
 
 # Check if we have the right args
 if [ "${args[0]}" != "init" ] && [ "${args[0]}" != "flush" ] && [ "${args[0]}" != "check" ] && [ "${args[0]}" != "ping" ]; then
-    echo "Usage: $0 <init | flush | check | ping2>"
+    echo "Usage: $0 <init | flush | check | ping>"
     exit 1
 fi
 
 # Check if blacklist file exists 
-if [ ! -f /etc/iptables/blacklist ]; then
+if [ ! -f $(pwd)/blacklist ]; then
     > $(pwd)/blacklist
 fi
 
@@ -35,29 +35,29 @@ fi
 # Check if we are flushing or initializing
 if [ ${args[0]} == "init" ]; then
     # Initialize the rules
-    iptables -N BLOCK
 
     for ASN in $ASNS; do
         echo "Fetching AS$ASN"
         curl -s "https://api.bgpview.io/asn/$ASN/prefixes" | jq -r '.data.ipv4_prefixes[].prefix' | grep -v null | anew -q $(pwd)/blacklist
-        sleep 0.5
+        sleep 0.3
     done
 
     # Aggregate the blacklist cidrs
+    O=$(cat $(pwd)/blacklist | wc -l)
     mv $(pwd)/blacklist $(pwd)/.blacklist_tmp
     cat $(pwd)/.blacklist_tmp | mapcidr -silent -a | anew -q $(pwd)/blacklist
+    A=$(cat $(pwd)/blacklist | wc -l)
+    rm -rf $(pwd)/.blacklist_tmp
 
-
-    iptables -F
-    iptables -P INPUT ACCEPT
-    iptables -P OUTPUT ACCEPT
-    iptables -P FORWARD ACCEPT
-
+    # Print the number of cidrs
+    echo "Blacklist contains $A aggregated cidrs (was $O)"
+    echo "Reduced by $(echo "scale=2; 100 - ($A / $O) * 100" | bc)%"
+    echo "Please wait while we add the rules to iptables"
     for IP in $(cat blacklist); do
-        echo "Blocking $IP"
         #iptables -A INPUT -s $IP -j DROP
         iptables -A OUTPUT -d $IP -j DROP
     done
+    echo "Done"
 
     #iptables-save
 
@@ -67,6 +67,8 @@ elif [ ${args[0]} == "flush" ]; then
     iptables -P INPUT ACCEPT
     iptables -P OUTPUT ACCEPT
     iptables -P FORWARD ACCEPT
+    iptables -X
+    echo "Flushed all rules"
 
 elif [ ${args[0]} == "check" ]; then
     # Check the rules
